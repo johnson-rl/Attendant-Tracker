629c7560451dbbdf970693fda0e4a5c71d0ab99e
johnson-rl <johnson.rl@gmail.com>
2017-02-25 11:49:53 -0800

    added a few routes, relationships working

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
-var express = require("express");
-var app = express();
-var path = require("path");
-var bodyParser = require("body-parser");
-var morgan = require('morgan');
+const express = require("express");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const morgan = require('morgan');
 
-var DB = require("./db/connection");
-var User = DB.models.User;
+const DB = require("./db/connection");
+const User = DB.models.User;
+const Attendant = DB.models.Attendant;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,18 +19,67 @@ app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:htt
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 
-app.post("/api/users", function(req, res){
-  User.create(req.body).then(function(user){
-    res.send(user)
-  });
-});
+//////////////////////////////
+//////******ROUTES******//////
+//////////////////////////////
 
+///////////////
+//USER ROUTES//
+///////////////
+
+// Get all users
 app.get("/api/users", function(req, res){
   User.findAll().then(function(user){
     res.json(user);
   });
 });
 
+// Create a user
+app.post("/api/users", function(req, res){
+  User.create(req.body).then(function(user){
+    res.send(user)
+  });
+});
+
+// Get one user
+app.get("/api/users/:id", function(req, res){
+  User.findById(req.params.id).then(function(user){
+    res.send(user)
+  });
+});
+
+////////////////////
+//ATTENDANT ROUTES//
+////////////////////
+
+// Get all attendants
+app.get("/api/attendants", function(req, res){
+  Attendant.findAll().then(function(attendant){
+    res.json(attendant);
+  });
+});
+
+// Get all attendants for one user
+app.get("/api/users/:id/attendants", function(req, res){
+  User.findById(req.params.id).then(function(user){
+    user.getAttendants().then(function(attendants){
+      res.send(attendants)
+    })
+  });
+});
+
+// Save an attendant
+app.post("/api/users/:id/attendants", function(req, res){
+  Attendant.create(req.body).then(function(attendant){
+    User.findById(req.params.id).then(function(user){
+      user.addAttendant(attendant);
+      res.send(attendant)
+    })
+  });
+});
+
+
+// Redirects all other routes for client side routing
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
