da65e82078adea12cf373053c903c6515561359e
johnson-rl <johnson.rl@gmail.com>
2017-02-27 01:40:55 -0800

    times rendering properly

--- src/components/calendar_day.js
+++ src/components/calendar_day.js
@@ -4,6 +4,14 @@ import CalendarEntry from './calendar_entry'
 const CalendarDay = ({events}) => {
   console.log('props', events)
   const calendarItems = events.map((event)=> {
+    let hour
+    if (event.time === 12){
+      event.time = (event.time+':00pm')
+    } else if (event.time > 12){
+      event.time = ((event.time-12)+':00pm')
+    } else {
+      event.time = (event.time+':00am')
+    }
     return (
       <CalendarEntry
         key={event.time}
