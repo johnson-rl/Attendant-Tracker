515edd3ded180d847f272d56fa6d705406890296
johnson-rl <johnson.rl@gmail.com>
2017-02-27 21:27:00 -0800

    input emiting to socket

--- server/app.js
+++ server/app.js
@@ -12,12 +12,24 @@ const Attendant = DB.models.Attendant;
 
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+
+// Fires when socket connection made
 io.on('connection', function(socket){
+
   console.log("you're on sockets")
+
+  // fires when room is hit
   socket.on('room', function(data) {
     console.log("you've reached room", data.room)
     socket.join(data.room);
   });
+
+  // first when text is entered
+  socket.on('text', function(data) {
+    socket.broadcast.to(data.room).emit('receive text',
+      data)
+      console.log('some dude wrote', data.text)
+  })
 });
 
 server.listen(3000);
--- src/components/room.js
+++ src/components/room.js
@@ -4,17 +4,33 @@ const io = require('socket.io-client')
 const socket = io()
 
 class Room extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+    // binds onInputChange to "this", which in this particular context is SearchBar
+    this.onInputChange = this.onInputChange.bind(this);
+    // this.onFormSubmit = this.onFormSubmit.bind(this); // also need to bind submit
+  }
+
   componentWillMount(){
     socket.emit('room', {room: 34})
 
   }
 
-
+  onInputChange(event) {
+    console.log(event.target.value);// without next line, state will not update
+    this.setState({term: event.target.value})
+    socket.emit('text', {
+    room: 34,
+    text: event.target.value
+  })
+  }
 
   render () {
     return (
       <div>
-        <input />
+        <input onChange={this.onInputChange}/>
       </div>
     )
   }
