75cdf576e9bf004551ceb332cd90552571090ad8
johnson-rl <johnson.rl@gmail.com>
2017-02-28 13:28:28 -0800

    migrated events model

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,16 +1,22 @@
-var Sequelize = require("sequelize");
-var sequelize = new Sequelize("postgres:///attendant");
-var User = sequelize.import("../models/user");
-var Attendant = sequelize.import("../models/attendant");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("postgres:///attendant");
+const User = sequelize.import("../models/user");
+const Attendant = sequelize.import("../models/attendant");
+const Event = sequelize.import("../models/events")
 
 Attendant.belongsTo(User);
 User.hasMany(Attendant);
+Event.hasMany(User);
+Event.hasMany(Attendant);
+Attendant.belongsTo(Event);
+User.belongsTo(Event);
 
 module.exports = {
   Sequelize: Sequelize,
   sequelize: sequelize,
   models: {
     User: User,
-    Attendant: Attendant
+    Attendant: Attendant,
+    Event: Event
   }
 }
