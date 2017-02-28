35247761c81cb7dcbb4c6855f71d10449ff061d7
johnson-rl <johnson.rl@gmail.com>
2017-02-27 23:58:59 -0800

    first draft of chat room done, everything working

--- src/components/room.js
+++ src/components/room.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 const io = require('socket.io-client')
 const socket = io()
@@ -7,9 +8,11 @@ class Room extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {term: ''};
+    this.state = {chat: [], term: ""};
     // binds onInputChange to "this", which in this particular context is SearchBar
     this.onInputChange = this.onInputChange.bind(this);
+    this.onInputSubmit = this.onInputSubmit.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
     // this.onFormSubmit = this.onFormSubmit.bind(this); // also need to bind submit
     socket.on('receive text', (payload) => {
         this.updateTextFromSockets(payload);
@@ -17,29 +20,58 @@ class Room extends Component {
     }
 
   updateTextFromSockets(payload) {
-    this.setState({term: payload.text})
+    this.setState({chat: [...this.state.chat, payload.text]})
+  }
+
+  scrollToBottom = () => {
+    const node = ReactDOM.findDOMNode(this.messagesEnd);
+    node.scrollIntoView({behavior: "smooth"});
   }
 
   componentWillMount(){
     socket.emit('room', {room: 34})
+  }
 
+  componentDidUpdate() {
+    this.scrollToBottom();
   }
 
   onInputChange(event) {
     this.setState({term: event.target.value})
-    socket.emit('text', {
-    room: 34,
-    text: event.target.value
-  })
   }
 
+  handleKeyPress(event) {
+    if(event.charCode==13){
+      event.preventDefault()
+      this.onInputSubmit()
+    }
+  }
+
+  onInputSubmit() {
+    socket.emit('text', {
+      room: 34,
+      text: this.state.term
+      })
+    this.setState({chat: [...this.state.chat, this.state.term], term: ""})
+  }
+
+
   render () {
+    const sentChats = this.state.chat.map((c, i)=>{
+      return (<p key={i}>{c}</p>)
+    })
+    
     return (
       <div>
         <div className="container">
           <div className="row">
-            <div className="eight columns offset-by-two chat-box">
-
+            <div className="ten columns offset-by-one">
+              <div className="container chat-box">
+                {sentChats}
+                <div style={ {float:"left", clear: "both"} }
+                  ref={(el) => { this.messagesEnd = el; }}>
+                </div>
+              </div>
             </div>
           </div>
           <div className="row">
@@ -48,11 +80,15 @@ class Room extends Component {
                 className="chat-input"
                 value={this.state.term}
                 onChange={this.onInputChange}
+                onKeyPress={this.handleKeyPress}
                 />
             </div>
           </div>
           <div className="center-text">
-            <button className="button-primary">Send</button>
+            <button
+              className="button-primary"
+              onClick={this.onInputSubmit}
+              >Send</button>
           </div>
 
         </div>
