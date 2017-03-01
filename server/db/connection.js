7138a5643c6c71a919da1bb6fb8b95a2f218becb
johnson-rl <johnson.rl@gmail.com>
2017-03-01 14:03:36 -0800

    reconfigured for heroku postgres db

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,5 +1,22 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("postgres:///attendant");
+if (!global.hasOwnProperty('db')) {
+  var Sequelize = require('sequelize')
+    , sequelize = null
+
+  if (process.env.HEROKU_POSTGRESQL_AMBER_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_AMBER_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+  } else {
+    // the application is executed on the local machine ... use mysql
+    sequelize = new Sequelize("postgres:///attendant")
+  }
+}
+
 const User = sequelize.import("../models/user");
 const Attendant = sequelize.import("../models/attendant");
 const Event = sequelize.import("../models/events")
