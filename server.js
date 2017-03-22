80de71b5cf33cfc3101bdd3bd6aa0f0a65cd7ba2
johnson-rl <johnson.rl@gmail.com>
2017-03-21 18:38:11 -0700

    set up remote pg

--- server.js
+++ server.js
@@ -1,32 +1,51 @@
-const Sequelize = require("sequelize");
+// const Sequelize = require("sequelize");
+//
+// var sequelize = new Sequelize('postgres:///attendant-dev', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   },
+// });
+//
+//
+// var User = sequelize.define('user', {
+//   firstName: {
+//     type: Sequelize.STRING,
+//   },
+//   lastName: {
+//     type: Sequelize.STRING
+//   }
+// }, {
+//   freezeTableName: true // Model tableName will be the same as the model name
+// });
+//
+// User.sync({force: true}).then(function () {
+//   // Table created
+//   return User.create({
+//     firstName: 'John',
+//     lastName: 'Hancock'
+//   });
+// });
 
-var sequelize = new Sequelize('postgres:///attendant-dev', {
-  host: 'localhost',
-  dialect: 'postgres',
 
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
+const http = require('http');
+const app = require('./app');
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const models = require('./server/models')
+
+models.sequelize.sync().then(function () {
+  server.listen(port);
+  server.on('error', onError);
+  server.on('listening', onListening);
 });
 
-
-var User = sequelize.define('user', {
-  firstName: {
-    type: Sequelize.STRING,
-  },
-  lastName: {
-    type: Sequelize.STRING
-  }
-}, {
-  freezeTableName: true // Model tableName will be the same as the model name
-});
-
-User.sync({force: true}).then(function () {
-  // Table created
-  return User.create({
-    firstName: 'John',
-    lastName: 'Hancock'
-  });
-});
+function onError(error) { console.log('server error') }
+function onListening() { console.log('you are now listening on', port) }
